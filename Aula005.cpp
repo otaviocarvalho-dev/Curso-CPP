70cd3a19d89dc5e8b6596fe3132a46ad3372183e
CARV4LH0-dev <otaviocarvalhodonascimento@gmail.com>
2024-10-25 13:31:52 -0300

    CursoCPP V1.1

---
+++ Aula005.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.1415;
+#define nome cout << "Otavio Carvalho do Nascimento\n";
+
+int main()
+{
+    int vidas = 3, tiros = 500, life = 100;
+    cout << pi;
+    nome
+
+    return 0;
+}
