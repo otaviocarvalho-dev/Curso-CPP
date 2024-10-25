7f0343fa6e05bea084fda2f325cfe1b69bc2b6f5
CARV4LH0-dev <otaviocarvalhodonascimento@gmail.com>
2024-10-25 13:05:27 -0300

    CursoCPP 1.0

---
+++ Aula004.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nome = "desconhecido";
+    int vidas = 0;
+    double coins = 0.0;
+
+    cout << "Digite o seu nome: ";
+    cin >> nome;
+
+    cout << "Digite o numero de vidas: ";
+    cin >> vidas;
+
+    cout << "Digite quantos coins o jogador tem: ";
+    cin >> coins;
+
+    cout << "\nNome: " << nome << "\nVidas: " << vidas << "\nCoins: " << coins;
+
+    return 0;
+}
