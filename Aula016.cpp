3c31d55c1bf59220baba0003a9e398d3c8874bcd
CARV4LH0-dev <otaviocarvalhodonascimento@gmail.com>
2024-10-29 00:05:36 -0300

    CursoCPP-V1.7

---
+++ Aula016.cpp
@@ -0,0 +1,18 @@
+// Comando "do while"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int cont;
+
+    cont = 0;
+    do
+    {
+        cout << cont << "\n";
+        cont++;
+    } while (cont < 10);
+
+    return 0;
+}
