f253dd7867b8cc9c03636300a9282ab847934f52
CARV4LH0-dev <otaviocarvalhodonascimento@gmail.com>
2024-11-01 20:08:06 -0300

    CursoCPP-V1.9

---
+++ Aula031.cpp
@@ -0,0 +1,36 @@
+// Lista (Parte 1)
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+    list<int> aula;
+    int tam = 10;
+    list<int>::iterator it;
+
+    aula.push_front(9);
+    aula.push_front(0);
+    aula.push_front(6);
+    aula.push_front(4);
+    aula.push_front(8);
+    aula.push_front(1);
+    aula.push_front(5);
+    aula.push_front(7);
+    aula.push_front(3);
+    aula.push_front(2);
+
+    cout << "Tamanho da lista: " << aula.size() << "\n";
+
+    aula.sort();
+    aula.reverse();
+
+    tam = aula.size();
+    for (int i = 0; i < tam; i++){
+        cout << aula.front() << "\n";
+        aula.pop_front();
+    }
+
+    return 0;
+}
