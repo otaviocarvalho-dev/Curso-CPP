682dfc388169e1d1392bf602198501be4af7b9bc
CARV4LH0-dev <otaviocarvalhodonascimento@gmail.com>
2024-11-05 00:12:53 -0300

    CursoCPP-V2.2

--- Aula041.cpp
+++ Aula041.cpp
@@ -6,9 +6,17 @@ using namespace std;
 
 int main()
 {
+    int n = 15;
     float pi = 3.1415;
 
-    printf("Valor de pi: %.2f", pi);
+    cout << "Valor do numero em decimal: " << n << "\n";
+    cout << "Valor do numero em hexadecimal: " << hex << n << "\n";
+
+    cout.precision(3);
+    cout << "Valor de pi: " << pi << "\n";
+
+    cout.precision(-1);
+    cout << "Valor de pi: " << pi << "\n";
 
     return 0;
 }
