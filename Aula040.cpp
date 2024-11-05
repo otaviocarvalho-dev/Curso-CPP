3f3f7a00082f150fbcb87b5e8bf82237e8dfc995
CARV4LH0-dev <otaviocarvalhodonascimento@gmail.com>
2024-11-04 23:53:09 -0300

    CursoCPP-V2.1

---
+++ Aula040.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int ano;
+    string nome;
+
+    printf("Digite seu nome: ");
+    scanf("%s", &nome);
+
+    printf("\nAno de nascimento: ");
+    scanf("%d", &ano);
+
+    printf("\nNome do usuario: %s - Ano de nascimento do usuario: %d", nome, ano);
+
+    return 0;
+}
+
+// "d" ou "i" = int
+// "x" ou "X"
+// "u" = int sem sinal
+// "s" = string, char*
+// "f" = double
+// "p" = ponteiro
